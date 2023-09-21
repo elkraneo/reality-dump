0760b7ce762fd04100787e2feb9797d92420f7b7
Cristian Díaz <cristian.diaz@monstar-lab.com>
2023-09-21 14:34:15 +0200

    feature: explore symbols properties

--- Tests/RealityDumpTests/RealityDumpTests.swift
+++ Tests/RealityDumpTests/RealityDumpTests.swift
@@ -3,71 +3,6 @@ import XCTest
 
 @testable import RealityDump
 
-/**
- ```
- swift symbolgraph-extract \
-  -module-name RealityFoundation \
-  -target arm64-apple-ios17.0 \
-  -output-dir ~/Desktop \
-  -pretty-print \
-  -sdk /Applications/Xcode-15.0.0-Release.Candidate.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS17.0.sdk
- ```
- */
-private func iOSSymbolGraph() -> SymbolGraph {
-  let url = Bundle.module.url(
-    forResource: "RealityFoundation.symbols",
-    withExtension: "json",
-    subdirectory: "iOS"
-  )!
-  let data = try! Data(contentsOf: url)
-  let symbolGraph = try! JSONDecoder().decode(SymbolGraph.self, from: data)
-  return symbolGraph
-}
-
-/**
- ```
- swift symbolgraph-extract \
-  -module-name RealityFoundation \
-  -target arm64-apple-macos14.0 \
-  -output-dir ~/Desktop \
-  -pretty-print \
-  -sdk /Applications/Xcode-15.0.0-Release.Candidate.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX14.0.sdk
- ```
- */
-private func macOSSymbolGraph() -> SymbolGraph {
-  let url = Bundle.module.url(
-    forResource: "RealityFoundation.symbols",
-    withExtension: "json",
-    subdirectory: "macOS"
-  )!
-  let data = try! Data(contentsOf: url)
-  let symbolGraph = try! JSONDecoder().decode(SymbolGraph.self, from: data)
-  return symbolGraph
-}
-
-/**
- ```
- swift symbolgraph-extract \
-  -module-name RealityFoundation \
-  -target arm64-apple-xros1.0 \
-  -output-dir ~/Desktop \
-  -pretty-print \
-  -sdk /Applications/Xcode-15.0.0-Beta.8.app/Contents/Developer/Platforms/XROS.platform/Developer/SDKs/XROS1.0.sdk
- ```
- */
-private func xrOSSymbolGraph() -> SymbolGraph {
-  let url = Bundle.module.url(
-    forResource: "RealityFoundation.symbols",
-    withExtension: "json",
-    subdirectory: "xrOS"
-  )!
-  let data = try! Data(contentsOf: url)
-  let symbolGraph = try! JSONDecoder().decode(SymbolGraph.self, from: data)
-  return symbolGraph
-}
-
-//MARK: -
-
 final class RealityDumpTests: XCTestCase {
 
   func test_components_iOS_symbolGraph() throws {
@@ -96,11 +31,7 @@ final class RealityDumpTests: XCTestCase {
     XCTAssertEqual(titles.count, 31)
     XCTAssertEqual(titles, expectedComponents_xrOS)
 
-    let n = extractEntities(from: xrOSSymbolGraph())
-      .map(\.names.title)
-      .sorted()
-    
-    dump(n)
+    _ = extractEntities(from: xrOSSymbolGraph())
   }
 
   // MARK: -
@@ -268,17 +199,112 @@ private func extractEntities(from symbolGraph: SymbolGraph) -> [SymbolGraph.Symb
       .first(where: { $0.identifier.precise == identifier })
   }
 
+  let properties = entities.map({ symbol in
+    symbolGraph.symbols.values
+      .filter({ $0.pathComponents.contains(symbol.names.title) })
+      .filter({ $0.kind.identifier == .property })
+      .compactMap(\.names.subHeading)
+      .map({ $0.map(\.spelling).reduce("", +) })
+  })
+  
+  for n in zip(entities, properties) {
+    dump(n.0.names.title)
+    dump(n.1)
+  }
+  
   return entities
+
+  //TODO: try to use mixins
+  /*
+   let symbol = try XCTUnwrap(symbolGraph.symbols.values.first)
+   let declaration = try XCTUnwrap(symbol.mixins[SymbolGraph.Symbol.DeclarationFragments.mixinKey] as? SymbolGraph.Symbol.DeclarationFragments)
+
+   XCTAssertTrue(declaration.declarationFragments.contains(where: { fragment in
+   fragment.kind == .keyword && fragment.spelling == "async"
+   }))
+
+   XCTAssertFalse(declaration.declarationFragments.contains(where: { fragment in
+   fragment.kind == .externalParameter && fragment.spelling == "completionHandler"
+   }))
+   */
 }
 
-//TODO: try to use mixins
-// let symbol = try XCTUnwrap(symbolGraph.symbols.values.first)
-// let declaration = try XCTUnwrap(symbol.mixins[SymbolGraph.Symbol.DeclarationFragments.mixinKey] as? SymbolGraph.Symbol.DeclarationFragments)
-//
-// XCTAssertTrue(declaration.declarationFragments.contains(where: { fragment in
-//     fragment.kind == .keyword && fragment.spelling == "async"
-// }))
-//
-// XCTAssertFalse(declaration.declarationFragments.contains(where: { fragment in
-//     fragment.kind == .externalParameter && fragment.spelling == "completionHandler"
-// }))
+// MARK: - Decoding
+
+/**
+ ```
+ swift symbolgraph-extract \
+  -module-name RealityFoundation \
+  -target arm64-apple-ios17.0 \
+  -output-dir ~/Desktop \
+  -pretty-print \
+  -sdk /Applications/Xcode-15.0.0-Release.Candidate.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS17.0.sdk
+ ```
+ */
+private func iOSSymbolGraph() -> SymbolGraph {
+  let url = Bundle.module.url(
+    forResource: "RealityFoundation.symbols",
+    withExtension: "json",
+    subdirectory: "iOS"
+  )!
+  let data = try! Data(contentsOf: url)
+  let symbolGraph = try! JSONDecoder().decode(SymbolGraph.self, from: data)
+  return symbolGraph
+}
+
+/**
+ ```
+ swift symbolgraph-extract \
+  -module-name RealityFoundation \
+  -target arm64-apple-macos14.0 \
+  -output-dir ~/Desktop \
+  -pretty-print \
+  -sdk /Applications/Xcode-15.0.0-Release.Candidate.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX14.0.sdk
+ ```
+ */
+private func macOSSymbolGraph() -> SymbolGraph {
+  let url = Bundle.module.url(
+    forResource: "RealityFoundation.symbols",
+    withExtension: "json",
+    subdirectory: "macOS"
+  )!
+  let data = try! Data(contentsOf: url)
+  let symbolGraph = try! JSONDecoder().decode(SymbolGraph.self, from: data)
+  return symbolGraph
+}
+
+/**
+ ```
+ swift symbolgraph-extract \
+  -module-name RealityFoundation \
+  -target arm64-apple-xros1.0 \
+  -output-dir ~/Desktop \
+  -pretty-print \
+  -sdk /Applications/Xcode-15.0.0-Beta.8.app/Contents/Developer/Platforms/XROS.platform/Developer/SDKs/XROS1.0.sdk
+ ```
+ */
+private func xrOSSymbolGraph() -> SymbolGraph {
+  let url = Bundle.module.url(
+    forResource: "RealityFoundation.symbols",
+    withExtension: "json",
+    subdirectory: "xrOS"
+  )!
+  let data = try! Data(contentsOf: url)
+  let symbolGraph = try! JSONDecoder().decode(SymbolGraph.self, from: data)
+
+  return symbolGraph
+
+  //TODO: explore `UnifiedSymbolGraph`
+  /*
+   let encoder = JSONEncoder()
+   encoder.outputFormatting.insert(.sortedKeys)
+   encoder.outputFormatting.insert(.prettyPrinted)
+
+   let a = try! encoder.encode(symbolGraph)
+   FileManager.default.createFile(atPath: "/Users/cristian/Desktop/a.json", contents: a)
+
+   let unified = UnifiedSymbolGraph.init(fromSingleGraph: symbolGraph, at: url)
+   let b = try! encoder.encode(unified)
+   FileManager.default.createFile(atPath: "/Users/cristian/Desktop/b.json", contents: b)
+   */
+}
